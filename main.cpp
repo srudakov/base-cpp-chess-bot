8f78345b37c75a7dff549176a6adda47089ff45a
Рудаков Сергей Андреевич <sergey.rudakov@nicetu.spb.ru>
2022-01-25 23:18:44 +0300

    validate move cells format

--- main.cpp
+++ main.cpp
@@ -19,13 +19,25 @@ namespace
   {
     return QJsonDocument::fromJson(message.toUtf8()).object();
   }
-  
+
+  QString validateCell(const QString& cell)
+  {
+    const QString& upperCell = cell.toUpper();
+    const int columnIndex = upperCell.indexOf(QRegExp("[A-H]"));
+    const int rowIndex = upperCell.indexOf(QRegExp("[1-8]"));
+    if((columnIndex < 0) || (rowIndex < 0))
+      return "";
+    return upperCell.mid(columnIndex, 1) + upperCell.mid(rowIndex, 1);
+  }
+
   void receiveMessage(const QString& message, BaseBot& bot)
   {
-    qInfo() << "message: " << message;
     const QJsonObject& values = parse(message);
     if(values.isEmpty() || !values.contains(MESSAGE_TYPE_KEY))
+    {
+      qWarning() << "invalid message: " << message;
       return;
+    }
     
     const QString& type = values.value(MESSAGE_TYPE_KEY).toString();
     if(type == "start_game")
@@ -33,20 +45,29 @@ namespace
       const bool white = values.value("color").toString().toLower() == QString("white");
       const double secsPerTurn = values.value("seconds_per_turn").toDouble(2.0);
       bot.startGame(white, secsPerTurn);
+      return;
     }
     if(type == "end_game")
     {
       const QString& winner = values.value("winner").toString();
       const QString& reason = values.value("reason").toString();
       qInfo() << "winner/reason: " << winner << "/" << reason;
+      return;
     }
     if(type == MOVE_TYPE)
     {
-      const QString& from = values.value(FROM_KEY).toString();
-      const QString& to = values.value(TO_KEY).toString();
-      qInfo() << "enemy move: " << from << " -> " << to;
-      bot.enemyMoved(from, to);
+      const QString& from = validateCell(values.value(FROM_KEY).toString());
+      const QString& to = validateCell(values.value(TO_KEY).toString());
+      if(from.isEmpty() || to.isEmpty())
+        qWarning() << "invalid move: " << message;
+      else
+      {
+        qInfo() << "enemy move: " << from << " -> " << to;
+        bot.enemyMoved(from, to);
+      }
+      return;
     }
+    qWarning() << "unknown message: " << message;
   }
   
   void register_on_server(QWebSocket& webSocket, const QString& name)
@@ -59,7 +80,11 @@ namespace
   void sendMove(QWebSocket& webSocket, const QString& from, const QString& to)
   {
     qInfo() << "move to send: " << from << " -> " << to;
-    const QJsonObject move{ {FROM_KEY, from}, {TO_KEY, to}, {MESSAGE_TYPE_KEY, MOVE_TYPE} };
+    const QString& checkedFrom = validateCell(from);
+    const QString& checkedTo = validateCell(to);
+    if(checkedFrom.isEmpty() || checkedTo.isEmpty())
+      return;
+    const QJsonObject move{ {FROM_KEY, checkedFrom}, {TO_KEY, checkedTo}, {MESSAGE_TYPE_KEY, MOVE_TYPE} };
     webSocket.sendBinaryMessage(QJsonDocument(move).toJson());
   }
 } // namespace
@@ -79,6 +104,7 @@ int main(int argc, char* argv[])
 
   QWebSocket webSocket;
   BaseBot bot([&webSocket](const QString& from, const QString& to) { sendMove(webSocket, from, to); });
+
   QObject::connect(&webSocket, &QWebSocket::connected,
                    &webSocket, [&webSocket, &name](){register_on_server(webSocket, name);});
   QObject::connect(&webSocket, &QWebSocket::textMessageReceived,
